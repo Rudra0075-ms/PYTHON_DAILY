20c2b0ea67da94192dd40cf90492eac3992c75d5
Rudra Madhab Sahoo <sahoorudramadhab2007@gmail.com>
2026-08-16 10:04:38 +0530

    Add files via upload

---
+++ string_revision/rotation_letters.py
@@ -0,0 +1,11 @@
+r= input("word1= ")
+r1= input("word2= ")
+if len(r)==len(r1) and r1 in r + r:
+    print("Rotation")
+else:
+    print("Not Rotation")    
+
+
+#word1= abcd
+#word2= cdab
+#Rotation
