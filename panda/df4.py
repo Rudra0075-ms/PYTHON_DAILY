a09bef87b5e634bf5f0e566d509eeb54c9c707b2
Rudra Madhab Sahoo <sahoorudramadhab2007@gmail.com>
2026-08-30 15:45:34 +0530

    Update df4.py

--- panda/df4.py
+++ panda/df4.py
@@ -1,17 +1,25 @@
 import pandas as pd
 
-data = {"Name": ["tom", "jerry", "spike"],"Age":[7,6,9]}
+data = {
+    "Name": ["tom", "jerry", "spike"],
+    "Age": [7, 6, 9]
+}
 
-df = pd.DataFrame(data, index = ["a","b","c"])
+df = pd.DataFrame(data, index=["a", "b", "c"])
 
 df["Job"] = ["cat", "mouse", "dog"]
 
-new_rows = pd.DataFrame([{"Name": "Billie", "Age": 28, "Job": "Engineer"},{"Name": "Hero", "Age": 18, "Job": "Chef"}], index=["d",""])
+new_rows = pd.DataFrame(
+    [
+        {"Name": "Billie", "Age": 28, "Job": "Gamer"},
+        {"Name": "Hero", "Age": 18, "Job": "Chef"}
+    ],
+    index=["d", "e"]
+)
 
 df = pd.concat([df, new_rows])
 
 print(df)
 
+print("\nFirst row:")
 print(df.iloc[0])
-
-
